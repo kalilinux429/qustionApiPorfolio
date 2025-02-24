44e29dbda106cc99e567be2233b13adba14934f3
Suchit Kapale <77877663+kalilinux429@users.noreply.github.com>
2025-02-24 23:28:10 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const questionRoutes = require('./routes/questionRoutes');
+const questionRoutes = require('./routes/QuestionRoutes');
 require('dotenv').config();
 
 const app = express();
